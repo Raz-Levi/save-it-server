cfe8da33c196643f0ab994f636b6ee0f696b8dcf
Raz Levi <razle102030@gmail.com>
2023-07-22 16:22:52 +0300

    Initialize Firestore

---
+++ firestore_init.py
@@ -0,0 +1,26 @@
+import firebase_admin
+from globals import project_configurations as config
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+def firestore_init():
+    cred = credentials.Certificate(config.ProjectConfigurations.get_service_account_key_path())
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+    data = {
+        "name": "Raz",
+        "age": 26,
+        "email": "john.doe@example.com",
+    }
+    db.collection("QA").document("QA").set(data)
+
+    # Read data from Firestore
+    user_ref = db.collection("QA").document("QA")
+    user_data = user_ref.get().to_dict()
+    print(user_data)
+
+
+if __name__ == '__main__':
+    firestore_init()
---
+++ globals/project_configurations.py
@@ -0,0 +1,4 @@
+class ProjectConfigurations:
+    @staticmethod
+    def get_service_account_key_path() -> str:
+        return "hidden_files/serviceAccountKey.json"
