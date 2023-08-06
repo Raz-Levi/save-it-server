31c56e5e2667fb9533af0d20b61e2ce87c97d8c7
Raz Levi <razle102030@gmail.com>
2023-08-06 22:00:41 +0300

    Integrate Swagger with Authentication Service

--- common/interface/service_interface.py
+++ common/interface/service_interface.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from flask import Flask, scaffold
+from flask import Flask
 import json
 from enum import Enum
 from typing import Any
@@ -11,7 +11,7 @@ class ServiceInterface(ABC):
     def __init__(self):
         self._app = Flask(self.service_name)
         self._app.json_encoder = EnumEncoder
-        self._api = Api(app=self._app, title=self.service_name, description=f'API for {self.service_name}', doc="/swagger")
+        self._api = Api(app=self._app, title=self.service_name, description=f'API for {self.service_name}', doc="/swagger", version="", )
 
         self._maintenance_namespace = self.define_namespace(namespace_name='maintenance', description='Maintenance Related Requests')
         self._api.add_namespace(self._maintenance_namespace)
